0b060d03af9a77c5998476e7abedaaa3deff2d1d
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2023-03-13 18:03:53 +0400

    update to v22.1

--- JS/src/app/richedit/richedit.component.ts
+++ JS/src/app/richedit/richedit.component.ts
@@ -1,7 +1,4 @@
-import {
-    create, createOptions, FileTabItemId, HomeTabItemId, Options,
-    RibbonButtonItem, RibbonItem, RibbonTab, RibbonTabType, RichEdit, RibbonMenuItem, RibbonSubMenuItem
-} from 'devexpress-richedit';
+import { create, createOptions, RichEdit, Options, RibbonTabType, FileTabItemId, HomeTabItemId, RibbonItem, RibbonSubMenuItem, RibbonMenuItem, RibbonButtonItem, FirstLevelRibbonItem, RibbonTab } from 'devexpress-richedit';
 
 import { AfterViewInit, Component, ElementRef, OnDestroy } from '@angular/core';
 
@@ -11,48 +8,48 @@ import { AfterViewInit, Component, ElementRef, OnDestroy } from '@angular/core';
   styleUrls: ['./richedit.component.css']
 })
 export class RicheditComponent implements AfterViewInit, OnDestroy {
-  private rich: RichEdit;
+  private rich: RichEdit | null = null;
 
   constructor(private element: ElementRef) { }
 
   ribbonCustomization(options: Options) {
     // remove items
     const fileTab = options.ribbon.getTab(RibbonTabType.File);
-    fileTab.removeItem(FileTabItemId.OpenDocument);
+    fileTab!.removeItem(FileTabItemId.OpenDocument);
 
     const homeTab = options.ribbon.getTab(RibbonTabType.Home);
-    homeTab.removeItem(HomeTabItemId.Copy);
-    homeTab.removeItem(HomeTabItemId.Cut);
-    homeTab.removeItem(HomeTabItemId.Paste);
+    homeTab!.removeItem(HomeTabItemId.Copy);
+    homeTab!.removeItem(HomeTabItemId.Cut);
+    homeTab!.removeItem(HomeTabItemId.Paste);
 
-    homeTab.removeItem(HomeTabItemId.DecreaseFontSize);
-    homeTab.removeItem(HomeTabItemId.IncreaseFontSize);
+    homeTab!.removeItem(HomeTabItemId.DecreaseFontSize);
+    homeTab!.removeItem(HomeTabItemId.IncreaseFontSize);
 
-    homeTab.removeItem(HomeTabItemId.ChangeStyle);
+    homeTab!.removeItem(HomeTabItemId.ChangeStyle);
 
     // remove element from drop down list
-    // fileTab.removeItem(FileTabItemId.DownloadRtf);
-    // fileTab.removeItem(FileTabItemId.DownloadTxt);
+    // fileTab!.removeItem(FileTabItemId.DownloadRtf);
+    // fileTab!.removeItem(FileTabItemId.DownloadTxt);
 
-    // allow only download as docx. Create out item from default items
-    const downloadDocxItem = fileTab.getItem(FileTabItemId.DownloadDocx) as RibbonSubMenuItem;
-    const downloadItem = fileTab.getItem(FileTabItemId.Download) as RibbonMenuItem;
-    fileTab.removeItem(FileTabItemId.Download);
+    // allow download in docx format only. Create an item based on the default item options.
+    const downloadDocxItem = fileTab!.getItem(FileTabItemId.DownloadDocx) as RibbonSubMenuItem;
+    const downloadItem = fileTab!.getItem(FileTabItemId.Download) as RibbonMenuItem;
+    fileTab!.removeItem(FileTabItemId.Download);
     // icons: https://js.devexpress.com/Documentation/Guide/Themes_and_Styles/Icons/
-    fileTab.insertItem(new RibbonButtonItem(downloadDocxItem.id, downloadItem.text, {icon: downloadItem.icon, showText: true}), 2);
+    fileTab!.insertItem(new RibbonButtonItem(downloadDocxItem.id, downloadItem.text, {icon: downloadItem.icon, showText: true}), 2);
 
     // move items to new tab
-    const findElem: RibbonItem = homeTab.getItem(HomeTabItemId.Find);
-    const replaceElem: RibbonItem = homeTab.getItem(HomeTabItemId.Replace);
-    homeTab.removeItem(findElem);
-    homeTab.removeItem(replaceElem);
+    const findElem: RibbonItem = homeTab!.getItem(HomeTabItemId.Find)!;
+    const replaceElem: RibbonItem = homeTab!.getItem(HomeTabItemId.Replace)!;
+    homeTab!.removeItem(findElem);
+    homeTab!.removeItem(replaceElem);
 
     // remove tab
     options.ribbon.removeTab(RibbonTabType.References);
 
     // insert tab
     const findTabId = 'FindTabId';
-    const newFindTab = options.ribbon.insertTab(new RibbonTab('Find', findTabId, [findElem, replaceElem]), 2);
+    const newFindTab = options.ribbon.insertTab(new RibbonTab('Find', findTabId, [findElem as FirstLevelRibbonItem, replaceElem as FirstLevelRibbonItem]), 2);
 
     // add custom item
     const findInGoogleId = 'FindInGoogleId';
@@ -69,7 +66,7 @@ export class RicheditComponent implements AfterViewInit, OnDestroy {
   }  
 
   ngAfterViewInit(): void {
-    const options: Options = createOptions();
+    const options = createOptions();
     this.ribbonCustomization(options);
 
     options.width = '1700px';
